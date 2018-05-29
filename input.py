4c195b471a56e1c1bac8940ec3597d7cb2174b31
bhaveshpandey08 <bhaveshpandey92@gmail.com>
2018-05-29 16:57:17 +0530

    ADDED NEW FILES

---
+++ input.py
@@ -0,0 +1,14 @@
+print("enter your name")
+x = input()
+print("Hello " + x)
+print("how are you today?")
+y = input()
+y = y.lower()
+if "good" in y:
+   print("Ok, good to hear that")
+elif "fine" in y:
+   print("Ok, good to hear that")
+elif "great" in y:
+   print("Ok, good to hear that")
+else:
+   print("Sorry to hear that")
